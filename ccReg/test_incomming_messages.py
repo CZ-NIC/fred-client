109f6e3b3a4cac2fb2b92501d865a3d0cfad38a1
Zdeněk Böhm <zdenek.bohm@nic.cz>
2006-09-27 14:56:09 +0000

    doplneni casti source (guiqt) a testovaci moduly
    git-svn-id: file:///data/svn/enum/epp_client/trunk@1449 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ ccReg/test_incomming_messages.py
@@ -0,0 +1,374 @@
+# -*- coding: utf8 -*-
+#!/usr/bin/env python
+"""TEST the server answers.
+"""
+data = (
+        ('greeting',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <greeting>
+    <svID>EPP server of cz.nic</svID>
+    <svDate>2006-07-12T08:52:01.0Z</svDate>
+    <svcMenu>
+      <version>1.0</version>
+      <lang>en</lang>
+      <lang>cs</lang>
+      <objURI>http://www.nic.cz/xml/epp/contact-1.0</objURI>
+      <objURI>http://www.nic.cz/xml/epp/domain-1.0</objURI>
+      <objURI>http://www.nic.cz/xml/epp/nsset-1.0</objURI>
+      <svcExtension>
+        <extURI>http://www.nic.cz/xml/epp/enumval-1.0</extURI>
+      </svcExtension>
+    </svcMenu>
+    <dcp>
+      <access>
+        <all/>
+      </access>
+      <statement>
+        <purpose>
+          <admin/>
+          <prov/>
+        </purpose>
+        <recipient>
+          <public/>
+        </recipient>
+        <retention>
+          <stated/>
+        </retention>
+      </statement>
+    </dcp>
+  </greeting>
+</epp>
+        """),
+        ('transfer',"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0"
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+     xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0
+     epp-1.0.xsd">
+  <command>
+    <transfer op="request">
+      <domain:transfer
+       xmlns:domain="http://www.nic.cz/xml/epp/domain-1.0"
+       xsi:schemaLocation="http://www.nic.cz/xml/epp/domain-1.0
+       domain-1.0.xsd">
+        <domain:name>example.cz</domain:name>
+        <domain:authInfo>
+            <domain:pw>2fooBAR</domain:pw>
+        </domain:authInfo>
+      </domain:transfer>
+    </transfer>
+    <clTRID>ABC-12345</clTRID>
+  </command>
+</epp>"""),
+    ('create:domain',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='2005'>
+      <msg lang='cs'>Chybná syntaxe hodnoty parametru</msg>
+      <extValue>
+        <value>
+          <registrant>pepa</registrant>
+        </value>
+        <reason lang='en'>unknow Registrant</reason>
+      </extValue>
+      <extValue>
+        <value>
+          <nsset/>
+        </value>
+        <reason>unknow nsset</reason>
+      </extValue>
+    </result>
+    <trID>
+      <clTRID>dnlq003#06-07-12at11:41:05</clTRID>
+      <svTRID>ccReg-0000010019</svTRID>
+    </trID>
+  </response>
+</epp>
+    """),
+    ('poll',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='2400'>
+      <msg lang='cs'>Příkaz selhal</msg>
+      <extValue>
+        <value>
+          <poll msgID='2' op='ack'/>
+        </value>
+        <reason>unknow msgID 2</reason>
+      </extValue>
+    </result>
+    <trID>
+      <clTRID>blig004#06-07-13at10:49:35</clTRID>
+      <svTRID>ccReg-0000010109</svTRID>
+    </trID>
+  </response>
+</epp>
+    """),
+    ('poll',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='1301'>
+      <msg lang='cs'>Příkaz úspěšně proveden; potvrď za účelem vyřazení z fronty</msg>
+    </result>
+    <msgQ count='1' id='4'>
+      <qDate>2006-07-13T10:31:00.0Z</qDate>
+      <msg>Je tady nejaka hlaska...</msg>
+    </msgQ>
+    <trID>
+      <clTRID>mubd002#06-07-13at13:49:00</clTRID>
+      <svTRID>ccReg-0000010147</svTRID>
+    </trID>
+  </response>
+</epp>
+    """),
+    ('contact:create',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:contact='http://www.nic.cz/xml/epp/contact-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='1000'>
+      <msg lang='cs'>Příkaz úspěšně proveden</msg>
+    </result>
+    <resData>
+      <contact:creData xsi:schemaLocation='http://www.nic.cz/xml/epp/contact-1.0 contact-1.0.xsd'>
+        <contact:id>lubosid</contact:id>
+        <contact:crDate>2006-07-14T09:02:59.0Z</contact:crDate>
+      </contact:creData>
+    </resData>
+    <trID>
+      <clTRID>ebvg003#06-07-14at11:02:58</clTRID>
+      <svTRID>ccReg-0000010885</svTRID>
+    </trID>
+  </response>
+</epp>"""),
+    ('domain:create',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:domain='http://www.nic.cz/xml/epp/domain-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='1000'>
+      <msg lang='cs'>Příkaz úspěšně proveden</msg>
+    </result>
+    <resData>
+      <domain:creData xsi:schemaLocation='http://www.nic.cz/xml/epp/domain-1.0 domain-1.0.xsd'>
+        <domain:name>lusk.cz</domain:name>
+        <domain:crDate>2006-07-14T11:25:55.0Z</domain:crDate>
+        <domain:exDate>2006-07-14T00:00:00.0Z</domain:exDate>
+      </domain:creData>
+    </resData>
+    <trID>
+      <clTRID>ausc002#06-07-14at13:25:54</clTRID>
+      <svTRID>ccReg-0000011001</svTRID>
+    </trID>
+  </response>
+</epp>
+"""),
+    ('domain:renew',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:domain='http://www.nic.cz/xml/epp/domain-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='1000'>
+      <msg lang='cs'>Příkaz úspěšně proveden</msg>
+    </result>
+    <resData>
+      <domain:renData xsi:schemaLocation='http://www.nic.cz/xml/epp/domain-1.0 domain-1.0.xsd'>
+        <domain:name>LUSK.CZ</domain:name>
+        <domain:exDate>2008-07-14T00:00:00.0Z</domain:exDate>
+      </domain:renData>
+    </resData>
+    <trID>
+      <clTRID>koix009#06-07-14at13:40:10</clTRID>
+      <svTRID>ccReg-0000011024</svTRID>
+    </trID>
+  </response>
+</epp>
+"""),
+    ('nsset:info',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:nsset='http://www.nic.cz/xml/epp/nsset-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='1000'>
+      <msg lang='cs'>Příkaz úspěšně proveden</msg>
+    </result>
+    <resData>
+      <nsset:infData xsi:schemaLocation='http://www.nic.cz/xml/epp/nsset-1.0 nsset-1.0.xsd'>
+        <nsset:id>one</nsset:id>
+        <nsset:roid>N0000000059-CZ</nsset:roid>
+        <nsset:status s='ok'/>
+        <nsset:clID>REG-LRR</nsset:clID>
+        <nsset:crDate>2006-06-30T09:09:57.0Z</nsset:crDate>
+        <nsset:upID>REG-LRR</nsset:upID>
+        <nsset:authInfo>
+          <nsset:pw>heslo</nsset:pw>
+        </nsset:authInfo>
+        <nsset:ns>
+          <nsset:name>ns1.one.cz</nsset:name>
+          <nsset:addr>192.23.54.1</nsset:addr>
+        </nsset:ns>
+        <nsset:ns>
+          <nsset:name>ns2.one.cz</nsset:name>
+          <nsset:addr>192.23.54.2</nsset:addr>
+          <nsset:addr>192.23.54.3</nsset:addr>
+          <nsset:addr>192.23.54.4</nsset:addr>
+          <nsset:addr>192.23.54.5</nsset:addr>
+        </nsset:ns>
+        <nsset:tech>NECOCZ-PETR</nsset:tech>
+      </nsset:infData>
+    </resData>
+    <trID>
+      <clTRID>ljmm002#06-07-17at10:36:13</clTRID>
+      <svTRID>ccReg-0000011281</svTRID>
+    </trID>
+  </response>
+</epp>
+"""),
+    ('contact:info',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:contact='http://www.nic.cz/xml/epp/contact-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='1000'>
+      <msg lang='cs'>Příkaz úspěšně proveden</msg>
+    </result>
+    <resData>
+      <contact:infData xsi:schemaLocation='http://www.nic.cz/xml/epp/contact-1.0 contact-1.0.xsd'>
+        <contact:id>test001</contact:id>
+        <contact:roid>C0000000031-CZ</contact:roid>
+        <contact:status s='ok'/>
+        <contact:postalInfo>
+          <contact:name>Řehoř Čížek</contact:name>
+          <contact:org>Čížková a spol</contact:org>
+          <contact:addr>
+            <contact:street>U práce</contact:street>
+            <contact:street>Za monitorem</contact:street>
+            <contact:street>Nad klávesnicí</contact:street>
+            <contact:city>Český Krumlov</contact:city>
+            <contact:sp>123</contact:sp>
+            <contact:pc>12300</contact:pc>
+            <contact:cc>CZ</contact:cc>
+          </contact:addr>
+        </contact:postalInfo>
+        <contact:voice>+123.456789</contact:voice>
+        <contact:fax>+321.564987</contact:fax>
+        <contact:email>rehor.cizek@mail.cz</contact:email>
+        <contact:crID>REG-LRR</contact:crID>
+        <contact:crDate>2006-08-01T13:21:16.0Z</contact:crDate>
+        <contact:disclose flag='0'>
+          <contact:fax/>
+          <contact:voice/>
+        </contact:disclose>
+        <contact:vat>963</contact:vat>
+        <contact:ssn>852</contact:ssn>
+        <contact:notifyEmail>info@rehorovi.cz</contact:notifyEmail>
+        </contact:infData>
+    </resData>
+    <trID>
+      <clTRID>hsrq003#06-08-01at15:58:42</clTRID>
+      <svTRID>ccReg-0000001553</svTRID>
+    </trID>
+  </response>
+</epp>
+    """),
+    ('domain:check',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:domain='http://www.nic.cz/xml/epp/domain-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='1000'>
+      <msg lang='cs'>Příkaz úspěšně proveden</msg>
+    </result>
+    <resData>
+      <domain:chkData xsi:schemaLocation='http://www.nic.cz/xml/epp/domain-1.0 domain-1.0.xsd'>
+        <domain:cd>
+          <domain:name avail='0'>nic.cz</domain:name>
+          <domain:reason>In use</domain:reason>
+        </domain:cd>
+        <domain:cd>
+        <domain:name avail='1'>myweb.cz</domain:name>
+        </domain:cd>
+      </domain:chkData>
+    </resData>
+    <trID>
+      <clTRID>cemf003#06-08-08at08:48:59</clTRID>
+      <svTRID>ccReg-0001323538</svTRID>
+    </trID>
+  </response>
+</epp>
+"""),
+    ('domain:create',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='2005'>
+      <msg lang='cs'>Chybná syntaxe hodnoty parametru</msg>
+      <extValue>
+        <value>
+          <registrant>reg-id</registrant>
+        </value>
+        <reason lang='cs'/>
+      </extValue>
+      <extValue>
+        <value>
+          <nsset>nsset1</nsset>
+        </value>
+        <reason lang='cs'>Hokus čuřil čeština</reason>
+      </extValue>
+    </result>
+    <trID>
+      <clTRID>qqve002#06-09-15at15:31:21</clTRID>
+      <svTRID>ccReg-0000180139</svTRID>
+    </trID>
+  </response>
+</epp>
+    """),
+    ('contact:list',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:contact='http://www.nic.cz/xml/epp/contact-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='1000'>
+      <msg lang='cs'>Příkaz úspěšně proveden</msg>
+    </result>
+    <resData>
+      <contact:listData xsi:schemaLocation='http://www.nic.cz/xml/epp/contact-1.0 contact-1.0.xsd'>
+        <contact:id>CID:FEELA</contact:id>
+        <contact:id>CID:TEST</contact:id>
+      </contact:listData>
+    </resData>
+    <trID>
+      <clTRID>jfbs002#06-09-21at17:27:25</clTRID>
+      <svTRID>ccReg-0000014445</svTRID>
+    </trID>
+  </response>
+</epp>
+"""),
+    ('nsset:list',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:nsset='http://www.nic.cz/xml/epp/nsset-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='1000'>
+      <msg lang='cs'>Příkaz úspěšně proveden</msg>
+    </result>
+    <resData>
+      <nsset:listData xsi:schemaLocation='http://www.nic.cz/xml/epp/nsset-1.0 nsset-1.0.xsd'>
+        <nsset:id>NSSID:NTEST</nsset:id>
+        <nsset:id>NSSID:AAA</nsset:id>
+        <nsset:id>NSSID:FEELA</nsset:id>
+        <nsset:id>NSSID:BBB</nsset:id>
+        <nsset:id>NSSID:CCC</nsset:id>
+      </nsset:listData>
+    </resData>
+    <trID>
+      <clTRID>jfbs003#06-09-21at17:28:34</clTRID>
+      <svTRID>ccReg-0000014446</svTRID>
+    </trID>
+  </response>
+</epp>
+"""),
+    ('domain:list',"""<?xml version='1.0' encoding='utf-8' standalone="no"?>
+<epp xmlns='urn:ietf:params:xml:ns:epp-1.0' xmlns:domain='http://www.nic.cz/xml/epp/domain-1.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd'>
+  <response>
+    <result code='1000'>
+      <msg lang='cs'>Příkaz úspěšně proveden</msg>
+    </result>
+    <resData>
+      <domain:listData xsi:schemaLocation='http://www.nic.cz/xml/epp/domain-1.0 domain-1.0.xsd'>
+        <domain:name>test.cz</domain:name>
+        <domain:name>2.1.1.5.4.7.2.2.2.0.2.4.e164.arpa</domain:name>
+        <domain:name>feela.cz</domain:name>
+        <domain:name>domain.cz</domain:name>
+      </domain:listData>
+    </resData>
+    <trID>
+      <clTRID>jfbs004#06-09-21at17:29:24</clTRID>
+      <svTRID>ccReg-0000014447</svTRID>
+    </trID>
+  </response>
+</epp>
+    """),
+    )
---
+++ ccReg/test_parser.py
@@ -0,0 +1,34 @@
+# -*- coding: utf8 -*-
+#!/usr/bin/env python
+
+import cmd_parser
+
+##dct_root =  {'command': [u'update_nsset']}
+##cols_root = [(u'update_nsset', (1, 1), (), '', ()), ('id', (1, 1), (), 'nsset ID', ()), ('add', (0, 1), (), '\xc4\x8d\xc3\xa1st add', (('dns', (0, 9), (), 'seznam DNS', (('name', (1, 1), (), 'jm\xc3\xa9no nssetu', ()), ('addr', (0, None), (), 'IP adresa', ()))), ('tech', (0, None), (), 'technick\xc3\xbd kontakt', ()), ('status', (0, 6), ('clientDeleteProhibited', 'clientTransferProhibited', 'clientUpdateProhibited', 'linked', 'ok'), 'status', ()))), ('rem', (0, 1), (), '\xc4\x8d\xc3\xa1st remove', (('name', (0, 9), (), 'jm\xc3\xa9no', ()), ('tech', (0, None), (), 'technick\xc3\xbd kontakt', ()), ('status', (0, 6), ('clientDeleteProhibited', 'clientTransferProhibited', 'clientUpdateProhibited', 'linked', 'ok'), 'status', ()))), ('chg', (0, 1), (), '\xc4\x8d\xc3\xa1st change', (('pw', (0, 1), (), 'heslo', ()),))]
+##text_line = 'update_nsset test02 --add.dns.addr (217.31.206.130, 217.31.206.129) --add.dns.name ns8.domain.cz'
+##text_line = 'update_nsset test02 --add.dns.name ns8.domain.cz --add.dns.addr (217.31.206.130, 217.31.206.129) '
+##text_line = 'update_nsset test02 --add.dns.addr (217.31.206.130, 217.31.206.129)'
+##text_line = 'update_nsset test02 --add.dns.addr 217.31.206.130 --add.dns.addr[1] 217.31.206.129'
+
+##dct_root = {'command': [u'create_contact']}
+##cols_root = [(u'create_contact', (1, 1), (), '', ()), ('contact_id', (1, 1), (), 'va\xc5\xa1e kontaktn\xc3\xad ID', ()), ('name', (1, 1), (), 'va\xc5\xa1e jm\xc3\xa9no', ()), ('email', (1, 1), (), 'v\xc3\xa1\xc5\xa1 email', ()), ('city', (1, 1), (), 'm\xc4\x9bsto', ()), ('cc', (1, 1), (), 'k\xc3\xb3d zem\xc4\x9b', ()), ('org', (0, 1), (), 'n\xc3\xa1zev organizace', ()), ('street', (0, 3), (), 'ulice', ()), ('sp', (0, 1), (), '\xc4\x8d.p.', ()), ('pc', (0, 1), (), 'PS\xc4\x8c', ()), ('voice', (0, 1), (), 'telefon', ()), ('fax', (0, 1), (), 'fax', ()), ('disclose_flag', (0, 1), ('y', 'n'), 'p\xc5\x99ep\xc3\xadna\xc4\x8d zve\xc5\x99ejnit (default y)', ()), ('disclose', (0, 6), ('name', 'org', 'addr', 'voice', 'fax', 'email'), 'nezve\xc5\x99ej\xc5\x88ovat', ()), ('vat', (0, 1), (), 'DPH', ()), ('ssn', (0, 1), (), 'SSN', ()), ('notify_email', (0, 1), (), 'ozn\xc3\xa1men\xc3\xad na email', ())]
+##text_line = 'create-contact regid --street (jedna dve tri) name mail city CZ'
+##text_line = 'create-contact regid --street (    ) name mail city CZ'
+##text_line = 'create-contact regid --street (jedna) name mail city CZ'
+##text_line = 'create-contact regid --street (jedna dve tri) name mail city CZ'
+##text_line = 'create-contact regid --street ("jedna safra" "dve cosi" "tri ano") name mail city CZ'
+##text_line = 'create-contact regid --street[1] (jedna dve tri) name mail city CZ'
+##text_line = 'create-contact regid --street ((jedna (dve tri) name mail city CZ'
+##text_line = 'create-contact regid --street (jedna dve (hokus pokus) tri) name mail city CZ'
+
+dct_root = {'command': [u'update_contact']}
+cols_root =  [(u'update_contact', (1, 1), (), '', ()), ('contact_id', (1, 1), (), 'va\xc5\xa1e kontaktn\xc3\xad ID', ()), ('add', (0, 5), ('clientDeleteProhibited', 'clientTransferProhibited', 'clientUpdateProhibited', 'linked', 'ok'), 'add status', ()), ('rem', (0, 5), ('clientDeleteProhibited', 'clientTransferProhibited', 'clientUpdateProhibited', 'linked', 'ok'), 'remove status', ()), ('chg', (0, 1), (), 'change status', (('postal_info', (0, 1), (), 'po\xc5\xa1tovn\xc3\xad informace', (('name', (0, 1), (), 'jm\xc3\xa9no', ()), ('org', (0, 1), (), 'n\xc3\xa1zev organizace', ()), ('addr', (0, 1), (), 'adresa', (('street', (0, 3), (), 'ulice', ()), ('city', (1, 1), (), 'm\xc4\x9bsto', ()), ('sp', (0, 1), (), '\xc4\x8d.p.', ()), ('pc', (0, 1), (), 'pc', ()), ('cc', (1, 1), (), 'cc', ()))))), ('voice', (0, 1), (), 'telefon', ()), ('fax', (0, 1), (), 'fax', ()), ('email', (0, 1), (), 'v\xc3\xa1\xc5\xa1 email', ()), ('disclose_flag', (0, 1), ('y', 'n'), 'p\xc5\x99ep\xc3\xadna\xc4\x8d zve\xc5\x99ejnit (default y)', ()), ('disclose', (0, 6), ('name', 'org', 'addr', 'voice', 'fax', 'email'), 'Zve\xc5\x99ejnit \xc3\xbadaje', ()), ('vat', (0, 1), (), 'DPH', ()), ('ssn', (0, 1), (), 'SSN', ()), ('notify_email', (0, 1), (), 'ozn\xc3\xa1men\xc3\xad na email', ())))]
+text_line = "update_contact myid '' '' (('' '' ((jedna, dve, tri) mesto '' '' CZ)))"
+
+print "="*60
+print "parse(dct_root, cols_root, text_line)"
+print "text_line:",text_line
+
+errors = cmd_parser.parse(dct_root, cols_root, text_line)
+print "errors:",errors
+print "dct_root:",dct_root
