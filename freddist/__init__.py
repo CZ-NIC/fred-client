1937af0f68aba946c407819f6162192fcf72253a
Aleš Doležal <ales.dolezal@nic.cz>
2008-05-12 16:31:35 +0000

    New version (with `freddist.file_util` module) added to `epp_client` and `pyfred`.
    git-svn-id: file:///data/svn/enum/epp_client/trunk@5341 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- freddist/__init__.py
+++ freddist/__init__.py
@@ -1,5 +1,6 @@
 __all__ = [
         'command',
         'core.py',
-        'dist.py'
+        'dist.py',
+        'file_util.py',
         ]
--- freddist/command/__init__.py
+++ freddist/command/__init__.py
@@ -10,5 +10,5 @@ __all__ = [
         'bdist.py',
         'bdist_rpm.py',
         'clean.py',
-        'uninstall.py'
+        'uninstall.py',
         ]
