26f1a9139a76eff0e0daf47c9b31710ce3e70e41
Jaromír Talíř <jaromir.talir@nic.cz>
2008-09-22 08:57:11 +0000

    release 2.0.1 preparation (changelog, version number update)
    git-svn-id: file:///data/svn/enum/client/trunk@6409 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- fred/internal_variables.py
+++ fred/internal_variables.py
@@ -25,7 +25,7 @@ It is used by client session Manager in session_base.py module.
 #---------------------------------------------------
 # Here is defined FRED VERSION:
 #---------------------------------------------------
-fred_version = '2.0.0'
+fred_version = '2.0.1'
 config_name = 'fred-client.conf'
 
 required_defaults = {'port':700, 'timeout':10.0}
