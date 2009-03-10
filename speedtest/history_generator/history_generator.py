44d0dbf377826dd4a4e181c5c88235fc50dc4a42
Tomáš Diviš <tomas.divis@nic.cz>
2009-03-10 14:34:38 +0000

    Added simple history generator and non-threaded speed tester.
    git-svn-id: file:///data/svn/enum/client/trunk@7455 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ speedtest/history_generator/history_generator.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+
+import time
+import datetime
+import random
+
+import fred
+
+OBJECT_NAMES = {
+    'contact': 'CID:%s',
+    'domain': '%s.cz',
+    'nsset': 'NSSID:%s',
+    'keyset': 'KEYID:%s',
+}
+
+class EppCommanderError(Exception):
+    pass 
+
+class PropabilityList(list):
+    ''' Inherited from python list, where items of this list must be couples (item, propability_weight),
+        added methond get_random_by_weight, which returns random first value of couple, with propability
+        computed as weight_of_item/sum_of_weights_of_all_items. 
+    ''' 
+    def get_random_key_by_weight(self):
+        weight_sum = sum(item[1] for item in self)
+        roll = random.randint(1, weight_sum)
+        sub_sum = 0
+        for i in range(len(self)):
+            sub_sum += self[i][1]
+            if roll <= sub_sum:
+                return self[i][0]
+        raise EppCommanderError('Wrong roll index or wrong content of PropabilityList') # should not ever occure
+
+def time_measure(func, *args, **kwd):
+    t1 = time.time()
+    result = func(*args, **kwd)
+    t_diff = time.time() - t1
+    return result, t_diff
+
+class EppCommander(object):
+    
+    def __init__(self):
+        self.epp = fred.Client()
+        self.epp.load_config()
+        self.epp.login(username = self.epp._epp.get_config_value('connect', 'username'), 
+                       password = self.epp._epp.get_config_value('connect', 'password'))
+        
+        #cache for all objects,:
+        self.cache = {
+            'contact': [],
+            'domain': [],
+            'nsset': [],
+            'keyset': [],
+        }
+        self.cache_valid = {
+            'contact': False,
+            'domain': False,
+            'nsset': False,
+            'keyset': False,
+        }
+    
+    def get_date_str(self):
+        dat = datetime.datetime.now()
+        dat_list = [dat.year, dat.month, dat.day, dat.hour, dat.minute, dat.second, dat.microsecond]
+        long_str = '-'.join([str(item) for item in dat_list])   
+        short_str = '-'.join([str(item) for item in dat_list[:3]])   
+        return long_str, short_str
+    
+    def get_random_handle(self, object_type):
+        ''' Returns random handle, object_type is string (e.g. 'contatct'). '''
+        if self.cache_valid[object_type]:
+            return random.choice(self.cache[object_type])
+        else: 
+            prep_objects = getattr(self.epp, 'prep_%ss' % object_type)
+            prep_objects()
+            objects = []
+            
+            result = self.epp.get_results()
+            while result['data']:
+                objects.extend(result['data']['list'])
+                result = self.epp.get_results()
+            
+            self.cache[object_type] = objects
+            self.cache_valid[object_type] = True
+            if not object:
+                raise EppCommanderError('There is no %s in database yet' % object_type)
+            return random.choice(objects)
+            
+        
+    def create(self, object_type, handle=None):
+        long_name, short_name = self.get_date_str()
+        if not handle:
+            handle = OBJECT_NAMES[object_type] % long_name
+            
+        if object_type == 'contact':
+            result, t_diff = time_measure(self.epp.create_contact,
+                handle,
+                'Maniasek D%s' % short_name,
+                'Maniasek@%s.cz' % short_name, 
+                'Zabredlicka', 'Bohumin', '22334', 'CZ'
+            )
+        elif object_type == 'nsset':
+            result, t_diff = time_measure(self.epp.create_nsset, handle, ({'name':'ns1.test.cz'}, {'name':'ns2.test.cz'}), self.get_random_handle('contact'))
+        elif object_type == 'keyset':    
+            result, t_diff = time_measure(self.epp.create_keyset,
+                'KEYID:%s' % long_name, 
+                [{'key_tag': '12345', 'alg': '5', 'digest_type': '1', 'digest': '1234ABCD1234ABCD1234ABCD1234ABCD1234ABCD'}], 
+                [], [], [], [self.get_random_handle('contact')]
+            )
+        elif object_type == 'domain':
+            registrant_handle = self.get_random_handle('contact')
+            result, t_diff = time_measure(self.epp.create_domain, '%s.cz' % long_name, registrant_handle)
+        else:
+            raise EppCommanderError('There is no such object type "%s"' % object_type)
+        
+        if result['code'] >= 2000:
+            raise EppCommanderError('Action create %s %s ended with errorcode %s, full result:\n %s' % (object_type, handle, result['code'], str(result)))
+        
+        self.cache_valid[object_type] = False
+        #print 'Created %s %s' % (object_type, handle)
+        
+        return t_diff
+        
+    def update(self, object_type, handle=None):
+        long_name, short_name = self.get_date_str()
+        if not handle:
+            handle = self.get_random_handle(object_type)
+            
+        if object_type == 'contact':
+            result, t_diff = time_measure(self.epp.update_contact, handle, chg={'email': '%s@updatle.cz' % short_name})
+        elif object_type == 'nsset':
+            result, t_diff = time_measure(self.epp.update_nsset, handle, auth_info='passwd' + long_name)
+        elif object_type == 'keyset':
+            result, t_diff = time_measure(self.epp.update_keyset, handle, auth_info='passwd' + long_name)
+        elif object_type == 'domain':
+            result, t_diff = time_measure(self.epp.update_domain, handle, chg={'auth_info': 'passwd' + long_name})
+        else:
+            raise EppCommanderError('There is no such object type "%s"' % object_type)
+        
+        if result['code'] >= 2000:
+            print result
+            raise EppCommanderError('Action update %s %s ended with errorcode %s, full result:\n %s' % (object_type, handle, result['code'], str(result)))
+        
+        #print 'Updated %s %s' % (object_type, handle)
+        return t_diff
+            
+    
+    def delete(self, object_type, handle=None):
+        if not handle:
+            handle = self.get_random_handle(object_type)
+        delete = getattr(self.epp, 'delete_%s' % object_type)
+        result, t_diff = time_measure(delete, handle)
+
+        if result['code'] >= 2000:
+            raise EppCommanderError('Action delete %s %s ended with errorcode %s, full result:\n %s' % (object_type, handle, result['code'], str(result)))
+        
+        print 'Deleted %s %s' % (object_type, handle)
+        return t_diff
+
+
+class EppGeneral(object):
+    def __init__(self, command_table=None):
+        self.eppc = EppCommander()
+        if not command_table:
+            # key is propability of command (command propability is actually key/sum_of_all_keys)
+            self.command_table = PropabilityList([
+                (self.eppc.create, 20),
+                (self.eppc.update, 75),
+                (self.eppc.delete, 10),
+            ])
+            self.object_table = PropabilityList([
+                ('contact', 30),
+                ('nsset', 20),
+                ('keyset', 10),
+                ('domain', 40),
+            ])
+        else:
+            self.command_table = command_table
+        
+        self.sum_command_table = None
+        self.sum_object_table = None
+            
+    def make_commands(self, count = 100):
+        for i in xrange(count):
+            
+            command = self.command_table.get_random_key_by_weight()
+            object = self.object_table.get_random_key_by_weight()
+            print 'Command %d: %s %s' % (i, command.im_func.func_name, object)
+            try:
+                command(object)
+            except EppCommanderError:
+                print 'Not accomplished due to EppError'
+        
+    
+        
+        
+        
+        
+if __name__ == "__main__":
+    general = EppGeneral()
+    #general.make_commands(100)        
+    
+    #commander = EppCommander()
+    #commander.create('nsset')
+    #commander.create('keyset')
+    #commander.create('domain')
+    #
+    #commander.update('contact')
+    #commander.update('nsset')
+    #commander.update('keyset')
+    #commander.update('domain')
+    #
+    #commander.delete('contact')
+    #commander.delete('nsset')
+    #commander.delete('keyset')
+    #commander.delete('domain')
+    
+    #print commander.get_random_handle('contact')
+    #print commander.get_random_handle('contact')
+    
+    print "Exiting..."
---
+++ speedtest/history_generator/speed_tester.py
@@ -0,0 +1,50 @@
+import time
+
+from history_generator import EppCommander, EppCommanderError
+
+class EppSpeedTester(object):
+    def __init__(self):
+        self.eppc = EppCommander()
+        self.times = {}
+        for command in ['create', 'update', 'delete']:
+            self.times[command] = {}
+            for obj in ['contact', 'nsset', 'keyset', 'domain']:
+                self.times[command][obj] = [] # list of times of how command for that object took long.
+
+    def print_result(self):
+        for command in ['create', 'update', 'delete']:
+            for obj in ['contact', 'nsset', 'keyset', 'domain']:
+                time_list = self.times[command][obj]
+                if time_list:
+                    avg_time = sum(time_list) / len(time_list)
+                    print '%-30s %.3f' % ('Avg. time for %s %s:' % (command, obj), avg_time)
+                    
+    def print_result_detailed(self):
+        for command in ['create', 'update', 'delete']:
+            for obj in ['contact', 'nsset', 'keyset', 'domain']:
+                time_list = self.times[command][obj]
+                if time_list:
+                    avg_time = sum(time_list) / len(time_list)
+                    min_time = min(time_list)
+                    max_time = max(time_list)
+                    print '%-30s %.3f   <%s, %s>' % ('Avg. time for %s %s:' % (command, obj), avg_time, min_time, max_time)
+                    #print '\n'.join(['%10.3f' % item for item in time_list])
+        
+    def measure_command(self, command, obj, count, *args, **kwd):
+        command_func = getattr(self.eppc, command)
+        for i in xrange(count):
+            try:
+                t_diff = command_func(obj, *args, **kwd)
+                self.times[command][obj].append(t_diff)
+            except EppCommanderError:
+                print 'Command not accomplished due to EppError (ignoring)'
+                
+
+if __name__ == '__main__':
+    est = EppSpeedTester()
+    #est.measure_command('update', 'contact', 10, 'CID:2009-3-6-11-49-56-376201')
+    est.measure_command('create', 'contact', 100)
+    
+    #est.print_result()
+    est.print_result_detailed()
+    
