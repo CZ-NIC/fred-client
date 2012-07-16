18ac9ab1d896222f0dfa667eceb374f9f81f8417
Tomas Divis <tomas.divis@nic.cz>
2012-07-16 12:08:03 +0200

    Ticket #7053 - shema_path is not needed anymore to run setup.py.

--- setup.py
+++ setup.py
@@ -55,20 +55,25 @@ if 'bdist_wininst' in sys.argv:
 
 
 
-def get_epp_schema_path(directory):
+def get_epp_schema_files(directory):
     "Return schemas path"
     global EPP_SCHEMAS_PATH
 
-    if 'EPP_SCHEMAS_PATH' in os.environ:
-        return os.environ['EPP_SCHEMAS_PATH']
+    schema_path = directory
+    if not schema_path:
+        if 'EPP_SCHEMAS_PATH' in os.environ:
+            schema_path = os.environ['EPP_SCHEMAS_PATH']
+        else:
+            for path in ('fred/schemas', '../../mod-eppd/trunk/schemas/', '../mod-eppd/schemas/'):
+                fullpath = os.path.normpath(os.path.join(directory, path))
+                if os.path.exists(fullpath):
+                    EPP_SCHEMAS_PATH = fullpath
+                    schema_path = fullpath
 
-    for path in ('fred/schemas', '../../mod-eppd/trunk/schemas/', '../mod-eppd/schemas/'):
-        fullpath = os.path.normpath(os.path.join(directory, path))
-        if os.path.exists(fullpath):
-            EPP_SCHEMAS_PATH = fullpath
-            return fullpath
-
-    raise IOError("Path to schemas missing.")
+    if schema_path:
+        return all_files_in_4(os.path.join('DATADIR', PACKAGE_NAME, 'schemas'), schema_path)
+    else:
+        return []
 
 
 
@@ -131,14 +136,14 @@ class EPPClientInstall(install):
     def __init__(self, *attrs):
         install.__init__(self, *attrs)
         # keep valid paths during package creation
-        self.is_bdist_mode = None 
+        self.is_bdist_mode = None
         self.keeppatt = None
         # check if object runs in bdist mode
         for dist in attrs:
             for name in dist.commands:
                 if name == 'bdist':
                     # it is bdist mode - we are on creating the package
-                    self.is_bdist_mode = 1 
+                    self.is_bdist_mode = 1
                 elif name == 'bdist_wininst':
                     # if we create package for windows, we change patterns
                     # as late in postinstall process
@@ -178,7 +183,7 @@ class EPPClientInstall(install):
             values.append(('FRED_CLIENT_HOST', self.host))
             values.append(('FRED_CLIENT_PORT', self.port))
             self.replace_pattern(
-                os.path.join(self.srcdir, 'conf', config_name+'.install'),
+                os.path.join(self.srcdir, 'conf', config_name + '.install'),
                 os.path.join('build', config_name),
                 values)
         else:
@@ -259,25 +264,25 @@ def main(directory):
     if os.environ.has_key('BDIST_SIMPLE'):
         APP_SCRIPTS.append(SCRIPT_FILENAME)
     try:
-        setup(name = PROJECT_NAME,
-            description = 'Client FRED (Free Registry for enum and domain)',
-            author = 'Zdenek Bohm, CZ.NIC',
-            author_email = 'zdenek.bohm@nic.cz',
-            url = 'http://www.nic.cz',
-            license = 'GNU GPL',
-            packages = ['fred','guiqt4'],
+        setup(name=PROJECT_NAME,
+            description='Client FRED (Free Registry for enum and domain)',
+            author='Zdenek Bohm, CZ.NIC',
+            author_email='zdenek.bohm@nic.cz',
+            url='http://www.nic.cz',
+            license='GNU GPL',
+            packages=['fred', 'guiqt4'],
             package_data={
-                'fred': ['INSTALL','LICENSE','CREDITS','*.txt',
+                'fred': ['INSTALL', 'LICENSE', 'CREDITS', '*.txt',
                     'lang/fred_client_cs.po',
                     'lang/cs/LC_MESSAGES/fred_client.mo'],
-                'guiqt4': ['*.py','*.png','*.qm'],
+                'guiqt4': ['*.py', '*.png', '*.qm'],
             },
-            scripts = APP_SCRIPTS, 
+            scripts=APP_SCRIPTS,
             data_files=[
                 ('DATAROOTDIR/%s' % PACKAGE_NAME, [
                     'doc/fred_howto_cs.html',
-                    'doc/niccz_console.ico', 
-                    'doc/niccz_gui.ico', 
+                    'doc/niccz_console.ico',
+                    'doc/niccz_gui.ico',
                     'doc/configure.ico',
                     'doc/help.ico',
                     'doc/README_EN.txt',
@@ -292,18 +297,17 @@ def main(directory):
                 # on windows:  ALLUSERSPROFILE = C:\Documents and Settings\All Users
                 # on windows if ALL... missing:  C:\Python25\ 
                 #get_etc_config_name()
-                ('SYSCONFDIR/fred', [os.path.join('build', config_name)]) 
+                ('SYSCONFDIR/fred', [os.path.join('build', config_name)])
                 ]
-            + all_files_in_4(os.path.join('DATADIR', PACKAGE_NAME, 'schemas'),
-                        get_epp_schema_path(directory)),
-            cmdclass = {
+            + get_epp_schema_files(directory),
+            cmdclass={
                     'build': EPPClientBuild,
                     'sdist': EPPClientSDist,
-                    'install': EPPClientInstall, 
+                    'install': EPPClientInstall,
                     'install_scripts': EPPClientInstall_scripts,
                     'install_lib':Install_lib,
                     'install_data':Install_data,
-                }, 
+                },
             )
         return True
     except OSError, msg:
