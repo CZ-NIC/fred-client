928b483942467e1c8bb9f2aa11386223c6f9c80e
Miroslav Franc <miroslav.franc@nic.cz>
2018-12-07 17:30:49 +0100

    Ticket #21315 - fix licenses

--- fred/fred_debug.py
+++ fred/fred_debug.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# For internal use only! It is important during make installation file.
-# Check this module if is not in installation. In case it is in, remove it.
+# Copyright (C) 2007-2018  CZ.NIC, z. s. p. o.
 #
+# This file is part of FRED.
+#
+# FRED is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# FRED is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FRED.  If not, see <https://www.gnu.org/licenses/>.
+
 """This module is used for test and debugging application.
 It is not uncluded in official release.
 """
