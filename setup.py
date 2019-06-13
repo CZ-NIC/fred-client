6836d094fabed868d19a4422136dc10ffe1d95c9
Jiri Sadek <jiri.sadek@nic.cz>
2019-06-13 13:38:20 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ class custom_sdist(sdist):
 
 
 setup(name='fred-client',
-      version='2.11.0',
+      version='2.12.2',
       description='Client FRED (Free Registry for enum and domain)',
       author='Zdenek Bohm, CZ.NIC',
       author_email='zdenek.bohm@nic.cz',
